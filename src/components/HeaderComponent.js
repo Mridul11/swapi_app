fb778cc03cda8eadb88d87f7123a8a3bf82fa292
Mridul11 <mridulmishra2991@gmail.com>
2019-12-09 12:17:07 +0530

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,7 @@
 import React from 'react' ;
 import { Header } from "semantic-ui-react";
+import {Link } from 'react-router-dom" ; 
 
-const HeaderComponent = () => <Header as="h1">HeaderComponent</Header>
+const HeaderComponent = () => <><Header as="h1">HeaderComponent</Header> <Link to="/login">login</Link> | <Link to="/dashboard">dashboard</Link>  </>
 
 export default HeaderComponent ; 
